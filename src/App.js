c38057b2618f08c9eeae9c9f20707f1d396dc70e
MomoZkan <garuburn99@gmail.com>
2020-05-31 14:33:12 +0900

    delete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <big>Edit <code>src/App.js</code> and save to reload.</big>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           <big>React Practice</big>
-        </a>
       </header>
     </div>
   );
