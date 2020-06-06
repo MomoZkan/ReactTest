79bed0283fc106e8d316d3e34b25a9925f4e3b7e
MomoZkan <garuburn99@gmail.com>
2020-06-06 23:18:39 +0900

    onClick実装　シャッフル未完成

--- src/Yamahuda.js
+++ src/Yamahuda.js
@@ -6,20 +6,20 @@ var myData = new Array(53);
 
 function shahhuru(){
   for (let i=0; i<53; i++) myData[i] = i;	// myData[0～30] に 1～31を順次セット
-  for (let i=0; i<500; i++){	// 500回 myData の内容をシャッフルする
-    let myA = Math.floor( Math.random() * 53 );	// 0～30 を決める
-    let myB = Math.floor( Math.random() * 53 );	// 0～30 を決める
-    let myBackup = myData[myA];	// myData の myA番目とmyB番目の内容を入れ替える
-    myData[myA] = myData[myB];	
-    myData[myB] = myBackup;	
-  }	
+  // for (let i=0; i<500; i++){	// 500回 myData の内容をシャッフルする
+  //   let myA = Math.floor( Math.random() * 53 );	// 0～30 を決める
+  //   let myB = Math.floor( Math.random() * 53 );	// 0～30 を決める
+  //   let myBackup = myData[myA];	// myData の myA番目とmyB番目の内容を入れ替える
+  //   myData[myA] = myData[myB];	
+  //   myData[myB] = myBackup;	
+  // }	
 }
 
-function hiku(){
-  const myReturn = myData[0];
-  for(let i=0; i<myData.length; i++)myData[i] = myData[i+1];
-  myData[53] = 0;
-  return alert("push!");
+function hiku(num){
+  // const myReturn = myData[0];
+  // for(let i=0; i<myData.length; i++)myData[i] = myData[i+1];
+  // myData[53] = 0;
+  alert(num);
   // return window.location.reload;
 }
 
@@ -27,9 +27,11 @@ function Yamahuda(){
   shahhuru();
   return(
     <div className="yamahuda">
-      {/* {myData.map((number) =>  */}
-      <Card className="cardMini" foo={myData[0]} onClick={alert("push!")} />
-      {/* )} */}
+      {myData.map((number) => 
+      <div onClick={(e)=>hiku(number)}>
+      <Card className="cardMini" foo={myData[number]} />
+      </div>
+      )}
     </div>
   );
 }
